fc360ecdaf31dc45151f56803598a07be5967454
Bashe Abdirahman <bashe.abdirahman@sedgwick.com>
2021-11-11 13:35:14 -0500

    second commit

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleApi.Controllers;
+
+namespace SimpleApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        WeatherForecastController weatherForecastController = new WeatherForecastController();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IEnumerable<WeatherForecast> returnValue = weatherForecastController.Get();
+            Assert.AreNotSame(-7, returnValue.ToList().OrderBy(c => c.TemperatureC).ElementAt(0).TemperatureC);
+        }
+    }
+}
